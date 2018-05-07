c849e2fe2fb1f2b9075fe943883a4765cc6f61a6
Pedro Martins <pbmartins@ua.pt>
2018-05-07 17:33:08 +0100

    Some changes on parsing script, now couting tcp sessions

--- parse_packets.py
+++ parse_packets.py
@@ -7,18 +7,19 @@ OUTFILE_PATH = 'mining_4t_nicehash.dat'
 SAMPLE_DELTA = 0.5
 
 LOCAL_IP = '192.168.1.158'
+LOCAL_IPV6 = '2001:8a0:de41:5501:9af7:5d3d:ed53:73dd'
 REMOTE_MINER_PORT = 3341
 
 def save_to_file(delta, last_bytes_up, last_bytes_down,
-        last_npkts_up, last_npkts_down):
+        last_npkts_up, last_npkts_down, last_nsyns_up, last_nsyns_down):
         global OUTFILE_PATH
 
         # Save to file
         with open(OUTFILE_PATH, "a") as f:
-            f.write("{} {} {} {}\n".format(last_bytes_up, last_bytes_down,
-                last_npkts_up, last_npkts_down))
+            f.write("{} {} {} {} {} {}\n".format(last_bytes_up, last_bytes_down,
+                last_npkts_up, last_npkts_down, last_nsyns_up, last_nsyns_down))
             for i in range(int(delta) - 1):
-                f.write("0 0 0 0\n");
+                f.write("0 0 0 0 0 0\n");
 
 
 def process_packets(tcp_cap):
@@ -29,51 +30,77 @@ def process_packets(tcp_cap):
     last_bytes_down = 0
     last_npkts_up = 0
     last_npkts_down = 0
+    last_nsyns_up = 0
+    last_nsyns_down = 0
 
     for packet in tcp_cap:
         packet_type = -1
-        if packet.ip.src == LOCAL_IP and \
+        if 'ipv6' in [l.layer_name for l in packet.layers]:
+            ip = LOCAL_IPV6
+            src = packet.ipv6.src
+            dst = packet.ipv6.dst
+        else:
+            ip = LOCAL_IP
+            src = packet.ip.src
+            dst = packet.ip.dst
+
+        if src == ip and \
                 int(packet.tcp.get_field('DstPort')) == REMOTE_MINER_PORT:
             packet_type = 0
         elif int(packet.tcp.get_field('SrcPort')) == REMOTE_MINER_PORT and \
-                packet.ip.dst == LOCAL_IP:
+                dst == ip:
             packet_type = 1
 
         if packet_type == 0:
             if last_timestamp is None:
                 last_timestamp = float(packet.sniff_timestamp)
-                last_bytes_down += int(packet.length)
+                last_bytes_down += int(packet.tcp.get_field('Len'))
                 last_npkts_down += 1
+                last_nsyns_down += 1 if packet.tcp.get_field(
+                        'Flags').main_field.hex_value & 18 == 18 else 0
             else:
                 time_delta = float(packet.sniff_timestamp) - last_timestamp
                 if time_delta > SAMPLE_DELTA:
                     save_to_file(time_delta, last_bytes_up, last_bytes_down,
-                            last_npkts_up, last_npkts_down)
+                            last_npkts_up, last_npkts_down, last_nsyns_up, 
+                            last_nsyns_down)
                     last_timestamp = float(packet.sniff_timestamp)
-                    last_bytes_down = int(packet.length)
+                    last_bytes_down = int(packet.tcp.get_field('Len'))
                     last_npkts_down = 1
+                    last_nsyns_down = 1 if packet.tcp.get_field(
+                            'Flags').main_field.hex_value & 18 == 18 else 0
                 else:
-                    last_bytes_down += int(packet.length)
+                    last_bytes_down += int(packet.tcp.get_field('Len'))
                     last_npkts_down += 1
+                    last_nsyns_down += 1 if packet.tcp.get_field(
+                            'Flags').main_field.hex_value & 18 == 18 else 0
         elif packet_type == 1:
             if last_timestamp is None:
                 last_timestamp = float(packet.sniff_timestamp)
-                last_bytes_up += int(packet.length)
+                last_bytes_up += int(packet.tcp.get_field('Len'))
                 last_npkts_up += 1
+                last_nsyns_up += 1 if packet.tcp.get_field(
+                        'Flags').main_field.hex_value & 18 == 18 else 0
             else:
                 time_delta = float(packet.sniff_timestamp) - last_timestamp
                 if time_delta > SAMPLE_DELTA:
                     save_to_file(time_delta, last_bytes_up, last_bytes_down,
-                            last_npkts_up, last_npkts_down)
+                            last_npkts_up, last_npkts_down, last_nsyns_up, 
+                            last_nsyns_down)
                     last_timestamp = float(packet.sniff_timestamp)
-                    last_bytes_up = int(packet.length)
+                    last_bytes_up = int(packet.tcp.get_field('Len'))
                     last_npkts_up = 1
+                    last_nsyns_up += 1 if packet.tcp.get_field(
+                            'Flags').main_field.hex_value & 18 == 18 else 0
                 else:
-                    last_bytes_up += int(packet.length)
+                    last_bytes_up += int(packet.tcp.get_field('Len'))
                     last_npkts_up += 1
+                    last_nsyns_up += 1 if packet.tcp.get_field(
+                            'Flags').main_field.hex_value & 18 == 18 else 0
 
 
-    save_to_file(0, last_bytes_up, last_bytes_down, last_npkts_up, last_npkts_down)
+    save_to_file(0, last_bytes_up, last_bytes_down, last_npkts_up, 
+            last_npkts_down, last_nsyns_up, last_nsyns_down)
 
 
 def main():
@@ -87,7 +114,7 @@ def main():
     parser.add_argument('-o', '--output', nargs='?',
             help='output processed file')
     parser.add_argument('-w', '--sampwindow', nargs='?',
-            help='sampling interval (default 1s)')
+            help='sampling interval (default 0.5s)')
     args = parser.parse_args()
 
     INFILE_PATH = args.input if args.input is not None else INFILE_PATH
