74edc4a32c9096ed181496692a3aede85d1fbea7
Pedro Martins <pbmartins@ua.pt>
2018-06-07 13:12:42 +0100

    Changed filtering to TCP flow only, ignored destination IP

--- filtering.py
+++ filtering.py
@@ -13,14 +13,12 @@ SAMPLE_DELTA = 0.5
 WINDOW_SIZE = 240
 N_WINDOWS = 5
 WINDOW_DELTA = WINDOW_SIZE * N_WINDOWS
-SRC_IP_ALLOCATE = 20
-DST_IP_ALLOCATE = 100
-TCP_PORT_ALLOCATE = 20
+SRC_IP_ALLOCATE = 40
+TCP_PORT_ALLOCATE = 100
 CLIENT_NETS_SET = None
 N_FEATURES = 5
 TRAFFIC_STATS = None
 LOCAL_IPS = {}
-REMOTE_IPS = {}
 TCP_PORTS = {}
 BASE_TIMESTAMP = None
 MINING_THRESHOLD = 0.5
@@ -29,7 +27,6 @@ MINING_THRESHOLD = 0.5
 def add_new_src_ip():
     global LOCAL_IPS
     global SRC_IP_ALLOCATE
-    global DST_IP_ALLOCATE
     global TCP_PORT_ALLOCATE
     global N_FEATURES
     global TRAFFIC_STATS
@@ -38,28 +35,22 @@ def add_new_src_ip():
         return
 
     # Pre allocate memory space
-    remote_ips_size = TRAFFIC_STATS.shape[1]
-    tcp_ports_size = TRAFFIC_STATS.shape[2]
-    new_entry = np.zeros((SRC_IP_ALLOCATE, DST_IP_ALLOCATE, remote_ips_size,
-                          tcp_ports_size, WINDOW_DELTA, N_FEATURES))
+    tcp_ports_size = TRAFFIC_STATS.shape[1]
+    new_entry = np.zeros((SRC_IP_ALLOCATE, tcp_ports_size, WINDOW_DELTA, N_FEATURES))
     TRAFFIC_STATS = np.vstack((TRAFFIC_STATS, new_entry))
 
 
-def add_new_dst_ip():
+def add_new_tcp_port():
     global LOCAL_IPS
-    global SRC_IP_ALLOCATE
-    global DST_IP_ALLOCATE
     global TCP_PORT_ALLOCATE
     global N_FEATURES
     global TRAFFIC_STATS
 
-    if len(REMOTE_IPS) < TRAFFIC_STATS.shape[1]:
+    if len(TCP_PORTS) < TRAFFIC_STATS.shape[1]:
         return
 
     # Pre allocate memory space
-    tcp_ports_size = TRAFFIC_STATS.shape[2]
-    new_entry = np.zeros((DST_IP_ALLOCATE, tcp_ports_size,
-                          WINDOW_DELTA, N_FEATURES))
+    new_entry = np.zeros((TCP_PORT_ALLOCATE, WINDOW_DELTA, N_FEATURES))
     new_stats = np.array([np.vstack((TRAFFIC_STATS[0], new_entry))])
 
     for ip in range(1, TRAFFIC_STATS.shape[0]):
@@ -69,50 +60,24 @@ def add_new_dst_ip():
     TRAFFIC_STATS = new_stats
 
 
-def add_new_tcp_port():
-    global LOCAL_IPS
-    global N_FEATURES
-    global TRAFFIC_STATS
-
-    if len(TCP_PORTS) < TRAFFIC_STATS.shape[2]:
-        return
-
-    # Pre allocate memory space
-    new_entry = np.zeros((TCP_PORT_ALLOCATE, WINDOW_DELTA, N_FEATURES))
-    new_stats = None
-
-    for src_ip in range(0, TRAFFIC_STATS.shape[0]):
-        src_stats = np.array([np.vstack((TRAFFIC_STATS[src_ip][0], new_entry))])
-
-        for dst_ip in range(1, TRAFFIC_STATS.shape[1]):
-            src_stats = np.vstack((
-                src_stats, [np.vstack((TRAFFIC_STATS[src_ip][dst_ip], new_entry))]))
-
-        new_stats = np.array([src_stats]) if src_ip == 0 \
-            else np.vstack((new_stats, [src_stats]))
-
-    TRAFFIC_STATS = new_stats
-
-
-def classify(local_ip, remote_ip, remote_port):
+def classify(local_ip, remote_port):
     global TRAFFIC_STATS
     global MINING_THRESHOLD
 
     src_idx = LOCAL_IPS[local_ip]
-    dst_idx = REMOTE_IPS[remote_ip]
     port_idx = TCP_PORTS[remote_port]
 
-    print("DST -> {}:{}".format(remote_ip, remote_port))
-    #print(TRAFFIC_STATS[src_idx][dst_idx][port_idx])
+    print("DST PORT -> {}".format(remote_port))
+    #print(TRAFFIC_STATS[src_idx][port_idx])
 
     # Traffic profiling
-    dataset = TRAFFIC_STATS[src_idx][dst_idx][port_idx][:, 1:]
+    dataset = TRAFFIC_STATS[src_idx][port_idx][:, 1:]
     f, fs, fw = extract_live_features(dataset)
     all_features = np.hstack((f, fs, fw))
 
     if all_features.shape[0] == 0:
-        print("No features could be extracted from TCP flow with src IP {} and "
-              "dst IP {} on port {}.".format(local_ip, remote_ip, remote_port))
+        print("No features could be extracted from TCP flow with src IP {} "
+              "on port {}.".format(local_ip, remote_port))
         return -1
 
     norm_pca_features = normalize_live_features(all_features)
@@ -121,9 +86,9 @@ def classify(local_ip, remote_ip, remote_port):
     classes = classify_live_data(norm_pca_features)
     if classes['min'] >= MINING_THRESHOLD:
         # Block in the firewall
-        print("TCP flow with src IP {} and dst IP {} is running mining "
+        print("TCP flow with src IP {} is running mining "
               "on port {} with {}% accuracy".format(
-            local_ip, remote_ip, remote_port, classes['min']*100))
+            local_ip, remote_port, classes['min']*100))
 
         return -1
 
@@ -136,7 +101,6 @@ def pkt_callback(pkt):
     global CLIENT_NETS_SET
     global N_PACKETS
     global LOCAL_IPS
-    global REMOTE_IPS
     global TCP_PORTS
     global BASE_TIMESTAMP
 
@@ -148,12 +112,10 @@ def pkt_callback(pkt):
     # Verify if it's a valid IP prefix
     if src_ip in CLIENT_NETS_SET:
         local_ip = src_ip
-        remote_ip = dst_ip
         remote_port = dst_port
         up_down = 0
     elif dst_ip in CLIENT_NETS_SET:
         local_ip = dst_ip
-        remote_ip = src_ip
         remote_port = src_port
         up_down = 1
     else:
@@ -163,10 +125,6 @@ def pkt_callback(pkt):
         LOCAL_IPS[local_ip] = len(LOCAL_IPS)
         add_new_src_ip()
 
-    if remote_ip not in REMOTE_IPS:
-        REMOTE_IPS[remote_ip] = len(REMOTE_IPS)
-        add_new_dst_ip()
-
     if remote_port not in TCP_PORTS:
         idx = None
         for port in TCP_PORTS:
@@ -175,9 +133,8 @@ def pkt_callback(pkt):
         add_new_tcp_port()
 
     src_idx = LOCAL_IPS[local_ip]
-    dst_idx = REMOTE_IPS[remote_ip]
     port_idx = TCP_PORTS[remote_port]
-    info = TRAFFIC_STATS[src_idx][dst_idx][port_idx]
+    info = TRAFFIC_STATS[src_idx][port_idx]
     N_PACKETS += 1
 
     BASE_TIMESTAMP = float(pkt.sniff_timestamp) if BASE_TIMESTAMP is None \
@@ -188,24 +145,22 @@ def pkt_callback(pkt):
     idx = 0 if time_delta == 0 else int(time_delta / SAMPLE_DELTA)
 
     if idx >= WINDOW_DELTA:
-        rtn = classify(local_ip, remote_ip, remote_port)
-        TRAFFIC_STATS[src_idx][dst_idx][port_idx] = \
-            np.zeros((WINDOW_DELTA, N_FEATURES))
-        TRAFFIC_STATS[src_idx][dst_idx][port_idx][0][0] = rtn
+        rtn = classify(local_ip, remote_port)
+        TRAFFIC_STATS[src_idx][port_idx] = np.zeros((WINDOW_DELTA, N_FEATURES))
+        TRAFFIC_STATS[src_idx][port_idx][0][0] = rtn
         idx = 0
         info[idx][0] = timestamp
 
     info[idx][1+up_down] += int(pkt.ip.get_field('Len'))
     info[idx][3+up_down] += 1
 
-    TRAFFIC_STATS[src_idx][dst_idx][port_idx] = info
+    TRAFFIC_STATS[src_idx][port_idx] = info
 
 
 def main():
     global CLIENT_NETS_SET
     global SAMPLE_DELTA
     global SRC_IP_ALLOCATE
-    global DST_IP_ALLOCATE
     global TCP_PORT_ALLOCATE
     global TRAFFIC_STATS
     global MINING_THRESHOLD
@@ -239,8 +194,7 @@ def main():
         else MINING_THRESHOLD
 
     TRAFFIC_STATS = \
-        np.zeros((SRC_IP_ALLOCATE, DST_IP_ALLOCATE, TCP_PORT_ALLOCATE,
-                  WINDOW_DELTA, N_FEATURES))
+        np.zeros((SRC_IP_ALLOCATE, TCP_PORT_ALLOCATE, WINDOW_DELTA, N_FEATURES))
 
     try:
         capture = pyshark.LiveCapture(interface=net_interface, bpf_filter='tcp')
